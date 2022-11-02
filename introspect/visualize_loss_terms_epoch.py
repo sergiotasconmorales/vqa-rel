cc8b205451826af6adfe317cff7ae4cae932d8f2
Sergio Tascon-Morales <sergio.tascon@correounivalle.edu.co>
2022-11-02 14:02:35 +0100

    i2: Adding configs to check variance

--- introspect/visualize_loss_terms_epoch.py
+++ introspect/visualize_loss_terms_epoch.py
@@ -1,11 +1,13 @@
 # Script to visualize the behavior of the loss terms (BCE and consistency term) for each epoch, for each iteration.
 
+import numpy as np
 import json
 from os.path import join as jp
 from matplotlib import pyplot as plt
 
-exp_name = '138'
+exp_name = '147'
 path_logs = '/home/sergio814/Documents/PhD/code/logs/lxmert/snap/vqa'
+gain = 0.001
 
 # open consistency_log.json
 with open(jp(path_logs, 'config_{}_hpc'.format(exp_name),'consistency_log.json')) as f:
@@ -23,8 +25,8 @@ fig, ax = plt.subplots(1, n_epochs, figsize=(10, 10))
 for i in range(n_epochs):
     assert len(consistency_log[str(i)]) == len(bce_log[str(i)])
     # plot both things on the same subplot
-    ax[i].plot(consistency_log[str(i)], label='consistency')
-    ax[i].plot(bce_log[str(i)], label='bce')
+    ax[i].plot(gain*np.array(consistency_log[str(i)]), label='consistency')
+    ax[i].plot(np.array(bce_log[str(i)]), label='bce')
     ax[i].set_title('Epoch ' + str(i))
     ax[i].legend()
 plt.show()
