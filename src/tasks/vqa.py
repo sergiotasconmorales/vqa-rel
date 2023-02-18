98343563432b18faa717adc878bf27fbb6ae6d66
Sergio Tascon Morales <s.tasconmorales@gmail.com>
2023-02-18 10:01:01 +0100

    i2: Removing print and starting loss from first epoch

--- src/tasks/vqa.py
+++ src/tasks/vqa.py
@@ -231,7 +231,7 @@ class VQA:
                 if 'cnst_fcn' in args: 
                     gain = getattr(args, 'gain')
                     cons_term = consistency_loss1(softmax(logit), torch.argmax(target, dim=1), rel, epoch, cnst_fcn = args.cnst_fcn)
-                    print(loss.item(), cons_term.item())
+                    #print(loss.item(), cons_term.item())
                     bce_log[epoch].append(loss.detach().cpu().item())
                     #loss = (loss + gain*cons_term)*logit.size(1) 
                     loss = (loss + gain*cons_term)
